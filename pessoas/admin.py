0ead3893f3fbc02faa0f608a543368bd975bc548
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2021-12-04 10:23:09 -0300

    :tada: my first project in django

---
+++ pessoas/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Pessoa
+
+
+# Register your models here.
+class ListandoPessoas(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email')
+    list_display_links = ('id', 'nome', 'email')
+    search_fields = ('nome', 'email')
+    list_per_page = 10
+
+
+admin.site.register(Pessoa, ListandoPessoas)
---
+++ receitas/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Receita
+
+# Create your views here.
+
+
+def index(request):
+    # objects.all tras todas as receitas
+    # order_by('parametro'), ordena as receitas de acordo com o campo definido
+    receitas = Receita.objects.order_by('-data_receita').filter(publicada=True)
+
+    dados = {
+        'receitas': receitas
+    }
+    return render(request, 'index.html', dados)
+
+
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_a_exibir = {
+        'receita': receita  # receita_id
+    }
+    return render(request, 'receita.html', receita_a_exibir)
